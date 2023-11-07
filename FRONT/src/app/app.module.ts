b3166329cf0a51989616b470ffd8e71f03f24851
Diogo Deconto <dideconto@gmail.com>
2023-11-07 18:20:48 -0300

    FRONT ECOMMERCE - Cadastro de produtos com Material e mensagem de aviso

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -1,35 +1,48 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatTableModule} from '@angular/material/table';
-
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatTableModule } from "@angular/material/table";
+import { MatCardModule } from "@angular/material/card";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProdutoListarComponent } from "./pages/produto/produto-listar/produto-listar.component";
 import { ProdutoCadastrarComponent } from "./pages/produto/produto-cadastrar/produto-cadastrar.component";
 import { FormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent, ProdutoListarComponent, ProdutoCadastrarComponent],
+  declarations: [
+    AppComponent,
+    ProdutoListarComponent,
+    ProdutoCadastrarComponent,
+  ],
   imports: [
-    BrowserModule, 
-    AppRoutingModule, 
-    HttpClientModule, 
-    FormsModule, 
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- FRONT/src/app/pages/produto/produto-cadastrar/produto-cadastrar.component.ts
+++ FRONT/src/app/pages/produto/produto-cadastrar/produto-cadastrar.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { Router } from "@angular/router";
 import { Categoria } from "src/app/models/categoria.model";
 import { Produto } from "src/app/models/produto.model";
@@ -17,20 +18,26 @@ export class ProdutoCadastrarComponent {
   categoriaId: number = 0;
   categorias: Categoria[] = [];
 
-  constructor(private client: HttpClient, private router: Router) {}
+  constructor(
+    private client: HttpClient,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.client.get<Categoria[]>("https://localhost:7195/api/categoria/listar").subscribe({
-      //A requição funcionou
-      next: (categorias) => {
-        console.table(categorias);
-        this.categorias = categorias;
-      },
-      //A requição não funcionou
-      error: (erro) => {
-        console.log(erro);
-      },
-    });
+    this.client
+      .get<Categoria[]>("https://localhost:7195/api/categoria/listar")
+      .subscribe({
+        //A requição funcionou
+        next: (categorias) => {
+          console.table(categorias);
+          this.categorias = categorias;
+        },
+        //A requição não funcionou
+        error: (erro) => {
+          console.log(erro);
+        },
+      });
   }
 
   cadastrar(): void {
@@ -42,15 +49,29 @@ export class ProdutoCadastrarComponent {
       categoriaId: this.categoriaId,
     };
 
-    this.client.post<Produto>("https://localhost:7195/api/produto/cadastrar", produto).subscribe({
-      //A requição funcionou
-      next: (produto) => {
-        this.router.navigate(["pages/produto/listar"]);
-      },
-      //A requição não funcionou
-      error: (erro) => {
-        console.log(erro);
-      },
-    });
+    this.client
+      .post<Produto>(
+        "https://localhost:7195/api/produto/cadastrar",
+        produto
+      )
+      .subscribe({
+        //A requição funcionou
+        next: (produto) => {
+          this.snackBar.open(
+            "Produto cadastrado com sucesso!!",
+            "E-commerce",
+            {
+              duration: 1500,
+              horizontalPosition: "right",
+              verticalPosition: "top",
+            }
+          );
+          this.router.navigate(["pages/produto/listar"]);
+        },
+        //A requição não funcionou
+        error: (erro) => {
+          console.log(erro);
+        },
+      });
   }
 }
